0641147bfcad074a2e266183dc1844c2f0efde85
mertcandev76 <mertcanadsiz.dev@gmail.com>
2025-04-10 22:27:56 +0300

    Add generic repository interfaces and configure entity mappings
    - Created IGenericDal<T> interface with standard CRUD and list methods
    - Added entity-specific interfaces inheriting from the generic interface

---
+++ DataAccessLayer/Abstract/IDestinationDal.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IDestinationDal:IGenericDal<Destination>
+    {
+        //void Insert(Destination destination);
+        //void Delete(Destination destination);
+        //void Update(Destination destination);
+        //List<Destination> GetList();
+
+
+
+
+    }
+}
